416c60a1d471be6aac840b8a14895c9393784803
Steffen Schuemann <s.schuemann@pobox.com>
2020-10-24 12:16:50 +0200

    refs #78, added test of __MAC_OS_X_VERSION_MIN_REQUIRED

--- include/ghc/fs_std_fwd.hpp
+++ include/ghc/fs_std_fwd.hpp
@@ -34,7 +34,7 @@
 #ifndef GHC_FILESYSTEM_STD_FWD_H
 #define GHC_FILESYSTEM_STD_FWD_H
 #if defined(__cplusplus) && __cplusplus >= 201703L && defined(__has_include)
-#if __has_include(<filesystem>)
+#if __has_include(<filesystem>) && (!defined(__MAC_OS_X_VERSION_MIN_REQUIRED) || __MAC_OS_X_VERSION_MIN_REQUIRED >= 101500)
 #define GHC_USE_STD_FS
 #include <filesystem>
 namespace fs {
--- include/ghc/fs_std_impl.hpp
+++ include/ghc/fs_std_impl.hpp
@@ -32,7 +32,7 @@
 // header to work.
 //---------------------------------------------------------------------------------------
 #if defined(__cplusplus) && __cplusplus >= 201703L && defined(__has_include)
-#if __has_include(<filesystem>)
+#if __has_include(<filesystem>) && (!defined(__MAC_OS_X_VERSION_MIN_REQUIRED) || __MAC_OS_X_VERSION_MIN_REQUIRED >= 101500)
 #define GHC_USE_STD_FS
 #endif
 #endif
