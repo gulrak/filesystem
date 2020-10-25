cf7890719fe8205b86cdb19be77e71d02e0af84e
Steffen Schuemann <s.schuemann@pobox.com>
2020-10-25 09:02:27 +0100

    refs #76, additional support for _MSVC_LANG

--- include/ghc/fs_std_impl.hpp
+++ include/ghc/fs_std_impl.hpp
@@ -31,7 +31,7 @@
 // The cpp has to include this before including fs_std_fwd.hpp directly or via a different
 // header to work.
 //---------------------------------------------------------------------------------------
-#if defined(__cplusplus) && __cplusplus >= 201703L && defined(__has_include)
+#if (defined(_MSVC_LANG) && _MSVC_LANG >= 201703L) || (defined(__cplusplus) && __cplusplus >= 201703L) && defined(__has_include)
 #if __has_include(<filesystem>) && (!defined(__MAC_OS_X_VERSION_MIN_REQUIRED) || __MAC_OS_X_VERSION_MIN_REQUIRED >= 101500)
 #define GHC_USE_STD_FS
 #endif
